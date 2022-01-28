9cc14b053f1b4b44a9d9a96e1ae46153668af64a
Yuan Xue <xueyuan@ip-192-168-182-229.ec2.internal>
2022-01-27 22:48:35 -0800

    fixed bug. rsync with options -aR

--- update_repo.sh
+++ update_repo.sh
@@ -4,47 +4,48 @@ readarray -t dirs <$SCRIPT_DIR/config
 
 case $1 in
     "pull")
-	echo "Pulling all config directories to ${SCRIPT_DIR}"
-	for i in "${dirs[@]}"
-	do
-	    dest="${SCRIPT_DIR}/${i#*/*/*/}"
-	    echo "${i} -> ${dest}"
-	    if [ -d $i ] || [ -f $i ]
-	    then
-		mkdir -p $(dirname "$dest")
-		rsync -ar ${i} ${dest}
-	    fi
-	done
-	;;
+        cd $HOME
+        echo "Pulling all config directories to ${SCRIPT_DIR}"
+        for i in "${dirs[@]}"
+        do
+            dest="${SCRIPT_DIR}"
+            src="${i#*/*/*/}"
+            echo "${i} -> ${dest}/${i#*/*/*/}"
+            if [ -d $i ] || [ -f $i ]
+            then
+                rsync -aR $src "${dest}/"
+            fi
+        done
+        ;;
     "push")
-	echo "Pushing from ${SCRIPT_DIR} to system configs"
-	for i in "${dirs[@]}"
-	do
-	    dest="${SCRIPT_DIR}/${i#*/*/*/}"
-	    echo "${dest} -> ${i}"
-	    if [ -d $dest ] || [ -f $dest ]
-	    then
-		mkdir -p $(dirname "$i")
-		rsync -ar ${dest} ${i}
-	    fi
-	done
-	;;
+        cd $SCRIPT_DIR
+        echo "Pushing from ${SCRIPT_DIR} to system configs"
+        for i in "${dirs[@]}"
+        do
+            dest="${i#*/*/*/}"
+            echo "${dest} -> ${i}"
+            if [ -d "${SCRIPT_DIR}/${dest}" ] || [ -f "${SCRIPT_DIR}/${dest}" ]
+            then
+                rsync -aR ${dest} ${HOME}
+            fi
+        done
+        ;;
     "clear")
-	echo "Clearing files from ${SCRIPT_DIR}"
-	for i in "${dirs[@]}"
-	do
-	    directory=${i#*/*/*/}
-	    split=(${directory//// })
-	    rootdir=${split[0]}
-	    dest="${SCRIPT_DIR}/$rootdir"
-	    if [ -d $dest ] || [ -f $dest ]
-	    then
-		echo ${dest}
-		rm -rf $dest
-	    fi
-	done
-	;;
+        echo "Clearing files from ${SCRIPT_DIR}"
+        for i in "${dirs[@]}"
+        do
+            directory=${i#*/*/*/}
+            split=(${directory//// })
+            rootdir=${split[0]}
+            dest="${SCRIPT_DIR}/$rootdir"
+            if [ -d $dest ] || [ -f $dest ]
+            then
+                echo ${dest}
+                rm -rf $dest
+            fi
+        done
+        ;;
     *)
-	echo "Wrong arguments supplied. Needs to be either 'push' or 'pull'"
-	;;
+        echo "Wrong arguments supplied. Needs to be either 'push' or 'pull'"
+        ;;
 esac
